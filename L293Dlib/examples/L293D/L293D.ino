d27a554185fc7026485e76941feaec9d9b76cce2
Jaswanth Ch <chennu.siva@gmail.com>
2025-02-19 19:43:06 +0530

    Update L293D.ino

--- L293Dlib/examples/L293D/L293D.ino
+++ L293Dlib/examples/L293D/L293D.ino
@@ -5,12 +5,12 @@
   The circuit diagram and the motor driver pinout is there in the L293Dlib folder.
   This example code is in the public domain.
   
-  Created 1 jan 2020
-  by Jaswanth Chennu
+  Created 1 January 2020
+  by Jaswanth Venkata Sai Chennu
   
 */
 #include <L293Dlib.h>                                        //include the L293D library
-L293Dlib motor(11,10,9);                                     //(enable,IN 1 or 3,IN 2 or 4)
+L293Dlib motor(11,10,9);                                     //(enable pin,IN1 or IN3,IN2 or IN4)
 
 void setup() {
   motor.motorSpeed(150);                                     //set the motor speed
@@ -18,7 +18,7 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
+  delay(1000);                                               //wait for 1 second
   motor.forward();                                           //turn the motor forward
   delay(1000);                                               //wait for 1 second
   motor.backward();                                          //then turn it backward
